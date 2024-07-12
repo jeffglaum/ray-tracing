8414a6075720359dfade9ca465df5bbc4852041d
Jeff Glaum <jeffglaum@live.com>
2024-07-13 06:05:03 +0900

    read vertices and index values from obj file

--- src/vbo.rs
+++ src/vbo.rs
@@ -15,7 +15,8 @@ impl Buffer {
         gl::BindBuffer(self.target, self.id);
     }
 
-    pub unsafe fn set_data<D>(&self, data: &[D], usage: GLuint) {
+    //pub unsafe fn set_data<D>(&self, data: &[D], usage: GLuint) {
+    pub unsafe fn set_data<D>(&self, data: Vec<D>, usage: GLuint) {
         self.bind();
         let (_, data_bytes, _) = data.align_to::<u8>();
         gl::BufferData(
